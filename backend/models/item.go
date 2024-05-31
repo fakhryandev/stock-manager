7b18159dc78bd0ae50acfd44af79e04b53a878a7
Muhammad Fakhryan Zulfahmi <fakhryandev@gmail.com>
2024-05-31 23:02:21 +0700

    remove id from table field

--- backend/models/item.go
+++ backend/models/item.go
@@ -1,12 +1,15 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+)
 
 type Item struct {
-	gorm.Model
-	Kode      string `json:"kode" gorm:"primaryKey; type:varchar(10)"`
-	Nama      string `json:"nama" gorm:"type:varchar(20); not null"`
-	Jumlah    uint   `json:"jumlah" gorm:"type: int"`
-	Deskripsi string `json:"deskripsi" gorm:"type:text"`
-	Status    bool   `gorm:"default:true"`
+	Kode      string    `json:"kode" gorm:"primaryKey; type:varchar(10)"`
+	Nama      string    `json:"nama" gorm:"type:varchar(20); not null"`
+	Jumlah    uint      `json:"jumlah" gorm:"type: int"`
+	Deskripsi string    `json:"deskripsi" gorm:"type:text"`
+	Status    bool      `json:"status" gorm:"default:true"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
